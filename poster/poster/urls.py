d5ca1f2ba29464f22394ab7b654e638d8dd5c81b
HardCoder <tin.ceraj@vingd.com>
2019-12-05 13:25:58 +0100

    admin

--- poster/poster/urls.py
+++ poster/poster/urls.py
@@ -24,6 +24,7 @@
 
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
+from django.contrib import admin
 from django.urls import path
 from rest_framework import routers, serializers, viewsets
 
